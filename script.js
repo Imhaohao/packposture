e8cd62c509332cb8af86d5d9a212a9b2f66c4cc6
Imhaohao <108702756+Imhaohao@users.noreply.github.com>
2024-07-24 23:53:49 -0700

    finished index

---
+++ script.js
@@ -0,0 +1,12 @@
+var clouds = document.getElementsByClassName("bgCloud");
+  setInterval(function() {
+  for (var i = 0; i < clouds.length; i++){
+    var currentLeft = parseInt(clouds[i].style.left);
+
+    if (currentLeft < window.innerWidth+1000) {
+        clouds[i].style.left = (currentLeft + 4) + "px";
+    } else {
+        clouds[i].style.left = "-150px";
+    }
+  }
+}, 20);
