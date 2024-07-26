71443dd301fa3c310851b5851fa0289652f6d240
Imhaohao <108702756+Imhaohao@users.noreply.github.com>
2024-07-26 10:32:26 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-var clouds = document.getElementsByClassName("bgCloud");
-  setInterval(function() {
-  for (var i = 0; i < clouds.length; i++){
-    var currentLeft = parseInt(clouds[i].style.left);
+// var clouds = document.getElementsByClassName("bgCloud");
+//   setInterval(function() {
+//   for (var i = 0; i < clouds.length; i++){
+//     var currentLeft = parseInt(clouds[i].style.left);
 
-    if (currentLeft < window.innerWidth+1000) {
-        clouds[i].style.left = (currentLeft + 4) + "px";
-    } else {
-        clouds[i].style.left = "-150px";
-    }
-  }
-}, 20);
+//     if (currentLeft < window.innerWidth+1600) {
+//         clouds[i].style.left = (currentLeft + 4) + "px";
+//     } else {
+//         clouds[i].style.left = "-150px";
+//     }
+//   }
+// }, 20);
